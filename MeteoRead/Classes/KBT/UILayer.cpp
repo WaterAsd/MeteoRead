c71c23795df44566a8487b1d983aabc7f8602255
kubota kenzi <nkc.kenji.kubota@gmail.com>
2016-05-11 14:14:27 +0900

    とりあえず変更したもの

--- MeteoRead/Classes/KBT/UILayer.cpp
+++ MeteoRead/Classes/KBT/UILayer.cpp
@@ -31,7 +31,6 @@ void UILayer::update(float delta)
 void UILayer::CreateSprite()
 {
 	Sprite* button = Sprite::create("button.png");
-	button->setAnchorPoint(Vec2(0.5, 0));
-	button->setPosition(Vec2(250, 45));
+	button->setPosition(Vec2(150, 90));
 	this->addChild(button);
 }
