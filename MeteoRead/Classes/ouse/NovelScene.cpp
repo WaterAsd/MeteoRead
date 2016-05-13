273bf2c2ad08d82d455eb5d11cc334c85dce8060
ouse takahiro <nkc.takahiro.ouse@gmail.com>
2016-05-13 14:57:19 +0900

    no message

--- MeteoRead/Classes/ouse/NovelScene.cpp
+++ MeteoRead/Classes/ouse/NovelScene.cpp
@@ -25,11 +25,20 @@ bool NovelScene::init()
 		return false;
 	}
 
+	mojicnt = false;
+	mojiframe = false;
+
 	Size visibleSize = Director::getInstance()->getVisibleSize();
 	Vec2 origin = Director::getInstance()->getVisibleOrigin();
 
 	//画像を表示
 	auto sprite = Sprite::create("HelloWorld.png");
+	background = Sprite::create("window2.png");
+	////中央に表示されるように座標を設定
+	background->setPosition(Point(visibleSize.width / 2 + origin.x, visibleSize.height / 6 + origin.y));
+	background->setScale(0.75);
+	////画像を追加　第2引数は表示する順番（背景だから一番下つまり0）
+	this->addChild(background, 3);
 
 	//表示位置
 	sprite->setPosition(Vec2(visibleSize.width / 2 + origin.x, visibleSize.height / 2 + origin.y));
