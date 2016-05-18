631bd6ff1aeb8b273a50f05fb2073e2d19c37f85
ouse takahiro <nkc.takahiro.ouse@gmail.com>
2016-05-18 15:00:42 +0900

    no message

--- MeteoRead/Classes/ouse/NovelScene.cpp
+++ MeteoRead/Classes/ouse/NovelScene.cpp
@@ -33,12 +33,12 @@ bool NovelScene::init()
 
 	//画像を表示
 	auto sprite = Sprite::create("HelloWorld.png");
-	background = Sprite::create("window2.png");
+	auto sprite = Sprite::create("window2.png");
 	////中央に表示されるように座標を設定
-	background->setPosition(Point(visibleSize.width / 2 + origin.x, visibleSize.height / 6 + origin.y));
-	background->setScale(0.75);
+	sprite->setPosition(Point(visibleSize.width / 2 + origin.x, visibleSize.height / 6 + origin.y));
+	sprite->setScale(0.75);
 	////画像を追加　第2引数は表示する順番（背景だから一番下つまり0）
-	this->addChild(background, 3);
+	this->addChild(sprite, 3);
 
 	//表示位置
 	sprite->setPosition(Vec2(visibleSize.width / 2 + origin.x, visibleSize.height / 2 + origin.y));
