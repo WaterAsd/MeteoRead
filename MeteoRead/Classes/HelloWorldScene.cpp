ed9b95144fd46dcb1619a82fbb3c9dd8d97a666b
paoda110 <nkc.keishi.oohasi@gmail.com>
2016-05-19 14:57:27 +0900

    sub

--- MeteoRead/Classes/HelloWorldScene.cpp
+++ MeteoRead/Classes/HelloWorldScene.cpp
@@ -4,23 +4,24 @@ USING_NS_CC;
 
 Scene* HelloWorld::createScene()
 {
-    // シーンを作成する
+    // 'scene' is an autorelease object
     auto scene = Scene::create();
     
-    // レイヤーを作成する
+    // 'layer' is an autorelease object
     auto layer = HelloWorld::create();
 
-    // レイヤーをシーンに追加する
+    // add layer as a child to scene
     scene->addChild(layer);
 
-    // 作成したシーンのポインターを返す
+    // return the scene
     return scene;
 }
 
-// 初期化メソッド
+// on "init" you need to initialize your instance
 bool HelloWorld::init()
 {
-    // 親であるLayerクラスの初期化
+    //////////////////////////////
+    // 1. super init first
     if ( !Layer::init() )
     {
         return false;
@@ -29,7 +30,11 @@ bool HelloWorld::init()
     Size visibleSize = Director::getInstance()->getVisibleSize();
     Vec2 origin = Director::getInstance()->getVisibleOrigin();
 
+    /////////////////////////////
+    // 2. add a menu item with "X" image, which is clicked to quit the program
+    //    you may modify it.
 
+    // add a "close" icon to exit the progress. it's an autorelease object
     auto closeItem = MenuItemImage::create(
                                            "CloseNormal.png",
                                            "CloseSelected.png",
@@ -38,25 +43,33 @@ bool HelloWorld::init()
 	closeItem->setPosition(Vec2(origin.x + visibleSize.width - closeItem->getContentSize().width/2 ,
                                 origin.y + closeItem->getContentSize().height/2));
 
+    // create menu, it's an autorelease object
     auto menu = Menu::create(closeItem, NULL);
     menu->setPosition(Vec2::ZERO);
     this->addChild(menu, 1);
 
+    /////////////////////////////
+    // 3. add your codes below...
+
+    // add a label shows "Hello World"
+    // create and initialize a label
     
     auto label = Label::createWithTTF("Hello World", "fonts/Marker Felt.ttf", 24);
     
+    // position the label on the center of the screen
     label->setPosition(Vec2(origin.x + visibleSize.width/2,
                             origin.y + visibleSize.height - label->getContentSize().height));
 
+    // add the label as a child to this layer
     this->addChild(label, 1);
 
-	//画像を表示
+    // add "HelloWorld" splash screen"
     auto sprite = Sprite::create("HelloWorld.png");
 
-	//表示位置
+    // position the sprite on the center of the screen
     sprite->setPosition(Vec2(visibleSize.width/2 + origin.x, visibleSize.height/2 + origin.y));
 
-	//レイヤーに追加
+    // add the sprite as a child to this layer
     this->addChild(sprite, 0);
     
     return true;
