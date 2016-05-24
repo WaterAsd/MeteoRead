a3f514ab8fcaef4593190e524d77fa0396f27f36
ouse takahiro <nkc.takahiro.ouse@gmail.com>
2016-05-24 15:04:17 +0900

    テキストが表示できた

--- MeteoRead/Classes/ouse/NovelScene.cpp
+++ MeteoRead/Classes/ouse/NovelScene.cpp
@@ -1,4 +1,6 @@
+#pragma execution_character_set("utf-8")//日本語が使えるようになる魔法の言葉
 #include "ouse/NovelScene.h"
+#include "ui/CocosGUI.h"
 
 USING_NS_CC;
 
@@ -23,15 +25,25 @@ bool NovelScene::init()
 		return false;
 	}
 
+	autoflg = false;
+	autoframe = false;
+	autocnt = false;
+	autospeed = false;
 	mojicnt = false;
 	mojiframe = false;
 
+	//画面サイズ取得
 	Size visibleSize = Director::getInstance()->getVisibleSize();
+	//マルチレゾリューション
 	Vec2 origin = Director::getInstance()->getVisibleOrigin();
 
+	auto textField = ui::TextField::create("ンゴ", "", 45);
+	textField->setPosition(Vec2(visibleSize.width * 0.5 + origin.x, visibleSize.height * 0.7 + origin.y));
+
+	this->addChild(textField);
+
 	//画像を表示
-	auto sprite = Sprite::create("HelloWorld.png");
-	//auto sprite = Sprite::create("window2.png");
+	auto sprite = Sprite::create("window2.png");
 	////中央に表示されるように座標を設定
 	sprite->setPosition(Point(visibleSize.width / 2 + origin.x, visibleSize.height / 6 + origin.y));
 	sprite->setScale(0.75);
